9837df2bc64dc6bdf320729f231d603d91e889ff
Kim Bedson <kimbedson@gmail.com>
2018-10-03 18:35:13 +1000

    finished notes controller fixed routes file

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,48 @@
 class NotesController < ApplicationController
+
+before_action :set_song
+  before_action :set_song_note, only: [:show, :update, :destroy]
+
+  # GET /songs/:song_id/notes
+  def index
+    json_response(@song.notes)
+  end
+
+  # GET /songs/:song_id/notes/:id
+  def show
+    json_response(@note)
+  end
+
+  # POST /songs/:song_id/notes
+  def create
+    @song.notes.create!(note_params)
+    json_response(@song, :created)
+  end
+
+  # PUT /songs/:song_id/notes/:id
+  def update
+    @song.update(note_params)
+    head :no_content
+  end
+
+  # DELETE /songs/:song_id/notes/:id
+  def destroy
+    @note.destroy
+    head :no_content
+  end
+
+  private
+
+  def note_params
+    params.permit(:name)
+  end
+
+  def set_song
+    @song = Song.find(params[:song_id])
+  end
+
+  def set_song_note
+    @note = @song.notes.find_by!(id: params[:id]) if @song
+  end
+end
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,32 @@
 class SongsController < ApplicationController
+
+    before_action :set_song, only: [:show, :update, :destroy]
+
+    #GET song
+    def index
+        @songs=Song.all
+        json_response(@songs)
+    end
+
+    def create
+        @song= Song.create!(song_params)
+    end
+
+    def show
+        json_response(@song)
+    end
+
+    def destroy
+        @song.destroy
+        head :no_content
+    end
+
+    def song_params
+        params.permit(:title,:created_by)
+    end
+
+    def set_song
+        @song= Song.find(params[:id])
+    end
+
 end
